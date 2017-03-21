ac9fd107eb456c2af64577f31ba354a77738ab0d
Aubrey Masten and Nick Powell <aubrey@>
2017-03-21 11:57:09 -0700

    create 3 column flashcard site with toggle functions.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('#flash1').click(function(){
+    $("#flash1 h2").toggle();
+    $("#flash1 p").toggle();
+  });
+
+  $('#flash2').click(function(){
+    $("#flash2 h2").toggle();
+    $("#flash2 p").toggle();
+  });
+  $('#flash3').click(function(){
+    $("#flash3 h2").toggle();
+    $("#flash3 p").toggle();
+  });
+});
