2d5449cb320a786a00de566303a9d8dfe05484dd
Aubrey Masten and Nick Powell <aubrey@>
2017-03-21 13:19:11 -0700

    add scalable javascript with jQuery & this

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
-  $('#flash1').click(function(){
-    $("#flash1 h2").toggle();
-    $("#flash1 p").toggle();
+  $("div.col-md-4").click(function(){
+    $(this).find("h2").toggle();
+    $(this).find("p").toggle();
   });
 
-  $('#flash2').click(function(){
-    $("#flash2 h2").toggle();
-    $("#flash2 p").toggle();
-  });
-  $('#flash3').click(function(){
-    $("#flash3 h2").toggle();
-    $("#flash3 p").toggle();
-  });
+  // $('#flash1').click(function(){
+  //   $("#flash1 h2").toggle();
+  //   $("#flash1 p").toggle();
+  // });
+  //
+  // $('#flash2').click(function(){
+  //   $("#flash2 h2").toggle();
+  //   $("#flash2 p").toggle();
+  // });
+  // $('#flash3').click(function(){
+  //   $("#flash3 h2").toggle();
+  //   $("#flash3 p").toggle();
+  // });
 });
